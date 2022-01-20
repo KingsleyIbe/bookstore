3755957f0783533e95bb03c2e9c08c912e153cac
Kingsley Ibe <43799727+KingsleyIbe@users.noreply.github.com>
2022-01-20 16:50:43 +0100

    Make modification to the api feature

--- src/utils/booksApi.js
+++ src/utils/booksApi.js
@@ -1,25 +1,44 @@
-import Axios from 'axios';
+import axios from 'axios';
 import { addBook, fetchBook, removeBook } from '../redux/actions/bookActions';
 
-const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/oF2DlchTxHbEjx4GHO3I/books';
+const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/zzjuKZTmzip2NB6sVBcS/books';
 
-// eslint-disable-next-line consistent-return
-export const addBookApi = (formData) => async (dispatch) => {
+/* eslint-disable */
+
+export const addBookApi = (AddNewBook) => async (dispatch) => {
   try {
-    await Axios.post(url, formData);
-    dispatch(addBook(formData));
+    const response = await fetch(`url ${AddNewBook}`, {
+      method: 'POST',
+      body: JSON.stringify(AddNewBook),
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    dispatch(addBook(AddNewBook));
+
   } catch (error) {
-    return error;
+    throw new Error(error.message);
   }
 };
 
-// eslint-disable-next-line consistent-return
+// export const addBookApi = (AddNewBook) => async (dispatch) => {
+//   try {
+//     await Axios.post('https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/zzjuKZTmzip2NB6sVBcS/books', AddNewBook);
+//     dispatch(addBook(AddNewBook));
+//   } catch (error) {
+//     return error;
+//   }
+// };
+
+
 export const fetchBookApi = () => async (dispatch) => {
   try {
-    const { data } = await Axios.get(url);
+    const { data } = await axios.get(url);
     const Books = Object.keys(data).map((key) => ({
       ...data[key][0],
-      book_id: key,
+      id: key,
     }));
 
     const payload = Object.values(Books);
@@ -29,12 +48,30 @@ export const fetchBookApi = () => async (dispatch) => {
   }
 };
 
-// eslint-disable-next-line consistent-return
-export const removeBookApi = (payload) => async (dispatch) => {
+
+// export const removeBookApi = (id) => async (dispatch) => {
+//   try {
+//     axios.delete(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/zzjuKZTmzip2NB6sVBcS/${payload}`);
+//     dispatch(removeBook(id));
+//   } catch (error) {
+//     return error;
+//   }
+// };
+
+
+export const removeBookApi = (id) => async (dispatch) => {
   try {
-    await Axios.delete(`url ${payload}`);
-    dispatch(removeBook(payload));
+    const response = await fetch(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/zzjuKZTmzip2NB6sVBcS/${id}`, {
+      method: 'DELETE',
+      mode: 'cors',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    dispatch(removeBook(id));
+
   } catch (error) {
-    return error;
+    throw new Error(error.message);
   }
 };
